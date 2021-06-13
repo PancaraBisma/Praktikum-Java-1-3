3b209f166a54b124a78d9daea5ffe5a457d32bdf
PancaraBisma <pancarabisma@gmail.com>
2021-06-13 15:16:55 +0800

    Membuat method hitung usia dan menampilkannya

--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -16,20 +16,18 @@ public class MahasiswaBeraksi {
         mahasiswa.nama = "Pancara";
         mahasiswa.tanggalLahir = simpleDateFormat.parse("10-09-2001");
 
-        System.out.println("Nama \t\t\t : " +mahasiswa.nama);
-        System.out.println("NPM \t\t\t : " +mahasiswa.npm);
-        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa.tanggalLahir));
-
         mahasiswa.tampilkanAtribut();
         mahasiswa.menyapa();
+        System.out.println("Usiaku \t\t\t : " +mahasiswa.hitungUsia() + " tahun");
 
         Mahasiswa mahasiswa2 = new Mahasiswa();
         mahasiswa2.npm = "19630000";
         mahasiswa2.nama = "Bisma";
         mahasiswa2.tanggalLahir = simpleDateFormat.parse("10-09-2000");
 
-        System.out.println("Nama \t\t\t : " +mahasiswa2.nama);
-        System.out.println("NPM \t\t\t : " +mahasiswa2.npm);
-        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa2.tanggalLahir));
+        mahasiswa2.tampilkanAtribut();
+        mahasiswa2.menyapa();
+        System.out.println("Usiaku \t\t\t : " +mahasiswa2.hitungUsia() + " tahun");
+
     }
 }
